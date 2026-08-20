568f50d67ec0e3ba5e5d2fd5878a3a5e25e4aeed
Aaron Fredrick <Aaron_Fredrick@proton.me>
2026-08-20 14:20:54 +0530

    Fix converter loading by registering programmatically
    - Changed XAML files back to Resource build action
    - Removed converter declaration from Converters.xaml
    - Added FileSizeConverter registration in App.xaml.cs constructor
    - This avoids XAML type resolution issues with shared converters

--- Chat.Client.Polling/App.xaml.cs
+++ Chat.Client.Polling/App.xaml.cs
@@ -1,8 +1,13 @@
 using System.Windows;
+using Chat.Client.Shared.Converters;
 
 namespace Chat.Client.Polling
 {
     public partial class App : Application
     {
+        public App()
+        {
+            Resources.Add("FileSizeConverter", new FileSizeConverter());
+        }
     }
 }
